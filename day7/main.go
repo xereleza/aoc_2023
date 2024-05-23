535ea120595799475257751f4d23d60a9a71f9f3
xere <xereleza@github.com>
2024-05-23 21:25:29 +0200

    day 7 part 1

--- day7/main.go
+++ day7/main.go
@@ -1,19 +1,71 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
+	"slices"
+	"strconv"
+	"strings"
 )
 
 type Hand struct {
-	cards string
-	rank  int
+	cards   string
+	rank    int
+	counter map[string]int
+	bid     int
 }
 
-func (h *Hand) setup() *Hand {
-	h.rank = 2
-	return h
+func (h Hand) setup() *Hand {
+	h.counter = make(map[string]int)
+	for _, card := range h.cards {
+		k := string(card)
+		i := h.counter[k]
+		h.counter[k] = i + 1
+	}
+	max := 1
+	var ref string
+	for k, v := range h.counter {
+		if v > max {
+			max = v
+			ref = k
+		}
+	}
+	switch max {
+	case 1:
+		h.rank = 1
+	case 2:
+		h.rank = checkTwoPair(h.counter, &ref)
+	case 3:
+		h.rank = checkFullHouse(h.counter, &ref)
+	default:
+		h.rank = max + 2
+	}
+
+	return &h
+}
+
+func checkTwoPair(counter map[string]int, ref *string) int {
+	for k, v := range counter {
+		if k == *ref {
+			continue
+		}
+		if v == 2 {
+			return 3
+		}
+	}
+	return 2
+}
+
+func checkFullHouse(counter map[string]int, ref *string) int {
+	for k, v := range counter {
+		if k == *ref {
+			continue
+		}
+		if v == 2 {
+			return 5
+		}
+	}
+	return 4
 }
 
 func main() {
@@ -25,15 +77,60 @@ func main() {
 	info, _ := file.Stat()
 	buf := make([]byte, info.Size())
 	file.Read(buf)
-	hand := &Hand{cards: "23456"}
-	fmt.Printf("%v\n", hand.rank)
-	fmt.Println(hand.rank)
-	hand.setup()
-	fmt.Println(hand.rank)
-	//contents := strings.Split(string(buf), "\n")
+	print(compute_game(parse_game(strings.Split(string(buf), "\n"))))
 }
 
-func compare(a string, b string) int {
+func compute_game(game []*Hand) int {
+	res := 0
+	slices.SortFunc(game, compare)
+	for i, hand := range game {
+		res += hand.bid * (i + 1)
+	}
+	return res
+}
+
+func parse_game(contents []string) []*Hand {
+	game := make([]*Hand, 0)
+	for _, hand := range contents {
+		_detail := strings.Split(hand, " ")
+		_bid, err := strconv.Atoi(_detail[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+		game = append(game, Hand{cards: _detail[0], bid: _bid}.setup())
+	}
+	return game
+}
+
+func compare(a *Hand, b *Hand) int {
 	//cmp(a, b) should return a negative number when a < b, a positive number when a > b and zero when a == b
+	if a.rank == b.rank {
+		return tiebreaker(a.cards, b.cards)
+	}
+	return a.rank - b.rank
+}
+
+func tiebreaker(a string, b string) int {
+	card_ranks := map[byte]int{
+		'A': 13,
+		'K': 12,
+		'Q': 11,
+		'J': 10,
+		'T': 9,
+		'9': 8,
+		'8': 7,
+		'7': 6,
+		'6': 5,
+		'5': 4,
+		'4': 3,
+		'3': 2,
+		'2': 1,
+	}
+	for i := range 5 {
+		if a[i] == b[i] {
+			continue
+		}
+		return card_ranks[a[i]] - card_ranks[b[i]]
+	}
 	return 0
 }
--- day7/main_test.go
+++ day7/main_test.go
@@ -12,6 +12,7 @@ func TestCompare(t *testing.T) {
 	tests := [][]Hand{
 		{Hand{cards: "AAA22"}, Hand{cards: "A22AA"}},
 		{Hand{cards: "22K34"}, Hand{cards: "AKQ34"}},
+		{Hand{cards: "AAAAA"}, Hand{cards: "AAAA4"}},
 	}
 
 	for _, tt := range tests {
@@ -20,8 +21,27 @@ func TestCompare(t *testing.T) {
 
 		testname := fmt.Sprintf("%s > %s", hand1.cards, hand2.cards)
 		t.Run(testname, func(t *testing.T) {
-			if hand1.rank <= hand2.rank {
+			if compare(hand1, hand2) <= 0 {
 				t.Errorf("%s <= %s", hand1.cards, hand2.cards)
+			}
+		})
+	}
+
+	// hand1 == hand2
+	tests = [][]Hand{
+		{Hand{cards: "AAA22"}, Hand{cards: "AAA22"}},
+		{Hand{cards: "22K34"}, Hand{cards: "22K34"}},
+		{Hand{cards: "AAAAA"}, Hand{cards: "AAAAA"}},
+	}
+
+	for _, tt := range tests {
+		hand1 := tt[0].setup()
+		hand2 := tt[1].setup()
+
+		testname := fmt.Sprintf("%s = %s", hand1.cards, hand2.cards)
+		t.Run(testname, func(t *testing.T) {
+			if compare(hand1, hand2) != 0 {
+				t.Errorf("%s != %s", hand1.cards, hand2.cards)
 			}
 		})
 	}
